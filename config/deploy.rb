40116e60028d3ce616bfa01e0601bc717a21d0d6
bishwas-py <bishwasbh@gmail.com>
2022-07-19 21:02:16 +0545

    Deploy now

--- config/deploy.rb
+++ config/deploy.rb
@@ -46,7 +46,7 @@ namespace :deploy do
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      invoke 'puma:restart'
+      execute :touch, release_path.join('tmp/restart.txt')
     end
   end
 end
