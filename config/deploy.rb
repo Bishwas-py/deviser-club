d4c65a00d95ba188d5a9c70b3d9ed54e8891382a
bishwas-py <bishwasbh@gmail.com>
2022-07-19 21:05:50 +0545

    Deploy now #3

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,48 +1,20 @@
-# config valid for current version and patch releases of Capistrano
 lock "~> 3.17.0"
 
 set :application, "deviser-club"
 set :repo_url, "git@github.com:Bishwas-py/deviser-club.git"
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-
-# Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, "/home/ubuntu/deviser-club"
 
 set :branch, ENV['BRANCH'] if ENV["BRANCH"]
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
 
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
 append :linked_files, "config/database.yml", 'config/master.key'
 
-# Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "tmp/webpacker", "public/system", "vendor", "storage"
 
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for local_user is ENV['USER']
-# set :local_user, -> { `git config user.name`.chomp }
-
-# Default value for keep_releases is 5
-set :keep_releases, 5
+set :keep_releases, 3
 set :keep_assets, 3
 
-set :db_local_clean, true
-set :db_remote_clean, true
-
 namespace :deploy do
-#  deploy:failed
-
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
@@ -52,6 +24,3 @@ namespace :deploy do
   after :publishing, 'deploy:restart'
   after :finishing, 'deploy:cleanup'
 end
-
-# Uncomment the following to require manually verifying the host key before first deploy.
-# set :ssh_options, verify_host_key: :secure
