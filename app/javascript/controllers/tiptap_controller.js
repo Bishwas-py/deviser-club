18fd9364709e7c531c48d42b6357b10f18ede5cb
bishwas-py <bishwasbh@gmail.com>
2022-07-24 20:28:26 +0545

    Activeness checking

--- app/javascript/controllers/tiptap_controller.js
+++ app/javascript/controllers/tiptap_controller.js
@@ -1,12 +1,14 @@
 import {Controller} from "@hotwired/stimulus"
 
-import { Editor } from '@tiptap/core'
+import {Editor} from '@tiptap/core'
 import StarterKit from '@tiptap/starter-kit'
+
+
+let textHandlers = ['bold', 'italic']
 
 // Connects to data-controller="tiptap"
 export default class extends Controller {
-    static targets = ['content', 'heading']
-
+    static targets = ['content', 'headingmarker', 'heading', ...textHandlers]
 
     connect() {
         this.editor = new Editor({
@@ -41,19 +43,65 @@ export default class extends Controller {
             `,
         })
     }
+
     initialize() {
-        console.log("hey")
+        ['click','input', 'keydown', 'keypress', 'change'].forEach( evt =>
+            {
+                this.element.addEventListener(evt, ()=> {
+                    {
+                        this.checkActive()
+                    }
+                })
+            }
+        );
     }
 
     boldText() {
         this.editor.chain().focus().toggleBold().run()
     }
+
     italicText() {
         this.editor.chain().focus().toggleItalic().run()
     }
+
     setHeading(event) {
         let level = event.target.dataset.headingLevel;
-        this.editor.chain().focus().toggleHeading({ level: parseInt(level) }).run()
+        this.editor.chain().focus().toggleHeading({level: parseInt(level)}).run()
+    }
+
+    checkActive(event) {
+        textHandlers.forEach(h => {
+            let normalButton = eval(`this.${h}Target`);
+            if (this.editor.isActive(h)) {
+                normalButton.style.backgroundColor = "transparent";
+                normalButton.style.border = "1px solid white";
+            } else {
+                normalButton.style.backgroundColor = null;
+                normalButton.style.border = null;
+            }
+        })
+        let numberOfInActiveHeadings = 0;
+        let headerButton = eval(this.headingmarkerTarget);
+        for (let i of Array(6).keys()) {
+            let headingName = `h${i+1}`;
+            if (!this.editor.isActive('heading', { level: i+1 })){
+                numberOfInActiveHeadings += 1;
+                let headingElement = this.headingTarget.querySelector(`#${headingName}`);
+                headingElement.style.backgroundColor = null;
+            } else {
+                let headingElement = this.headingTarget.querySelector(`#${headingName}`);
+                headingElement.style.backgroundColor = "purple";
+            }
+        }
+        if (numberOfInActiveHeadings === 6) {
+            headerButton.style.backgroundColor = null;
+            headerButton.classList.remove('ring-1')
+            headerButton.classList.remove('ring-white/50')
+        } else {
+            headerButton.classList.add('ring-1')
+            headerButton.style.backgroundColor = "transparent";
+            headerButton.classList.add('ring-white/50')
+        }
     }
 
     // makeText(event) {
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
 
   # resources :quick_tweets
   root "quick_tweets#index"
+  get 'global', to: 'global#index'
   resources :quick_tweets, path: :tweet
   resources :quick_tweets do
     resources :comments
