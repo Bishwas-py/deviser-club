23998be0fa318c5b11ae239b0d6b594df73a25c4
bishwas-py <bishwasbh@gmail.com>
2022-07-30 07:41:15 +0545

    comments working

--- app/javascript/controllers/comment_controller.js
+++ app/javascript/controllers/comment_controller.js
@@ -4,17 +4,16 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   initialize() {
   }
-  connect() {}
+  connect() {
+    this.element.addEventListener('click', this.toggleComment.bind(this))
+  }
   toggleComment(event) {
     event.preventDefault()
     event.stopPropagation()
-    console.log("dataset: ", event.target.dataset)
-    const commentBodyID = event.target.dataset.commentId;
-    console.log("commentBodyID: " + commentBodyID)
+    const commentBodyID = "comment-box";
     const commentBody = document.getElementById(commentBodyID);
     commentBody.classList.toggle('hidden');
 
-    console.log("this.element " + this.element)
     this.element.classList.toggle('bg-blue-300');
     this.element.classList.toggle('hover:bg-blue-400');
   }
