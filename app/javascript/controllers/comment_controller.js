a45c74bd5334b0d1df7d7ad00e2317c194b5d3c7
bishwas-py <bishwasbh@gmail.com>
2022-07-30 08:04:44 +0545

    New UI

--- app/javascript/controllers/comment_controller.js
+++ app/javascript/controllers/comment_controller.js
@@ -13,8 +13,5 @@ export default class extends Controller {
     const commentBodyID = "comment-box";
     const commentBody = document.getElementById(commentBodyID);
     commentBody.classList.toggle('hidden');
-
-    this.element.classList.toggle('bg-blue-300');
-    this.element.classList.toggle('hover:bg-blue-400');
   }
 }
