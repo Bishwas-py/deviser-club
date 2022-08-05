640061e640261e7710f2f1edd1cc71cd731fd314
bishwas-py <bishwasbh@gmail.com>
2022-08-05 08:07:36 +0545

    Host added in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
   # }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_options = {from: 'no-reply@deviser.club'}
+  config.action_mailer.default_options = {:host => "deviser.club", from: 'no-reply@deviser.club'}
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
