009a0ccb56cf7ccd1a0f328f65f624c75fb43f0d
bishwas-py <bishwasbh@gmail.com>
2022-08-05 09:26:10 +0545

    raise_delivery_errors

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,7 +45,6 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,6 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
 
   # POST FIX
   config.action_mailer.delivery_method = :sendmail
