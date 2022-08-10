56bd1f39cc477e4d5507744c24879bdef2be7d3d
bishwas-py <bishwasbh@gmail.com>
2022-08-10 08:49:36 +0545

    Proper ordering of Bookmarks

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,4 +2,6 @@ class Bookmark < ApplicationRecord
   validates :user_id, uniqueness: { scope: [:bookmarkable_id, :bookmarkable_type] }
   belongs_to :user
   belongs_to :bookmarkable, polymorphic: true
+
+  default_scope { order(created_at: :desc) }
 end
